acf80c4fc611895e9625f6b546ee65acb2a0e3b2
svanheulen <svanheulen@gmail.com>
2014-08-05 15:54:45 -0400

    initial commit

---
+++ messages/settings.py
@@ -0,0 +1,74 @@
+search = {
+    'greater_less': b"You have a hunch that the lock's combination is \x0c\x01[greater/less] than \n\x00.\x7f1\x00\x07",
+    'second_even_odd': b'You have a hunch that the second digit is \x0c\x00[even/odd].\x7f1\x00\x07',
+    'first_even_odd': b'You have a hunch that the first digit is \x0c\x00[even/odd].\x7f1\x00\x07',
+    'range': b'You have a hunch that the combination is greater than \n\x00 and less than \n\x01.\x7f1\x00\x07',
+    'less': b'You have a hunch that the combination is less than \n\x00.\x7f1\x00\x07',
+    'greater': b'You have a hunch that the combination is greater than \n\x00.\x7f1\x00\x07',
+    'equal': b'You have a hunch that one of the two digits is \n\x00.\x7f1\x00\x07',
+    'second_multiple': b'You have a hunch that the second digit is \n\x00, \n\x01, or \n\x02.\x7f1\x00\x07',
+    'first_multiple': b'You have a hunch that the first digit is \n\x00, \n\x01, or \n\x02.\x7f1\x00\x07',
+    'success': b'\x1c\x00 succeeded in opening the lock!\x7f1\x00\x07',
+    'failure': b'\x1c\x00 failed to open the lock.\x7f1\x00\x07'
+}
+
+zones = {
+    100: 6520, # west ronfaure
+    101: 6521, # east ronfaure
+    102: 6522, # la theine plateau
+    103: 6523, # valkurm dunes
+    104: 6524, # jugner forest
+    105: 6525, # batallia downs
+    106: 6526, # north gustaberg
+    107: 6527, # south gustaberg
+    108: 6528, # konschtat highlands
+    109: 6529, # pashhow marshlands
+    110: 6530, # rolanberry fields
+    111: 6531, # beaucedine glacier
+    112: 6532, # xarcabard
+    113: 6533, # cape teriggan
+    114: 6534, # eastern altepa desert
+    115: 6535, # west sarutabaruta
+    116: 6536, # east sarutabaruta
+    117: 6537, # tahrongi canyon
+    118: 6538, # buburimu peninsula
+    119: 6539, # meriphataud mountains
+    120: 6540, # sauromugue champaign
+    121: 6541, # the sanctuary of zi'tah
+    122: 6542, # ro'maeve
+    123: 6543, # yuhtunga jungle
+    124: 6544, # yhoator jungle
+    125: 6545, # western altepa desert
+    126: 6546, # qufim island
+    127: 6547, # behemoth's dominion
+    128: 6548, # valley of sorrows
+    130: 6550, # ru'aun gardens
+    153: 6573, # the boyahda tree
+    158: 6578, # upper delkfutt's tower
+    159: 6579, # temple of uggalepih
+    160: 6580, # den of rancor
+    166: 6586, # ranguemont pass
+    167: 6587, # bostaunieux oubliette
+    169: 6589, # torimarai canal
+    172: 6592, # zeruhn mines
+    173: 6593, # korroloka tunnel
+    174: 6594, # kuftal tunnel
+    176: 6596, # sea serpent grotto
+    177: 6597, # ve'lugannon palace
+    178: 6598, # the shrine of ru'avitau
+    190: 6610, # king ranperre's tomb
+    191: 6611, # dangruf wadi
+    192: 6612, # inner horutoto ruins
+    193: 6613, # ordelle's caves
+    194: 6614, # outer horutoto ruins
+    195: 6615, # the eldieme necropolis
+    196: 6616, # gusgan mines
+    197: 6617, # crawler's nest
+    198: 6618, # maze of shakhrami
+    200: 6620, # garlaige citadel
+    204: 6624, # fei'yin
+    205: 6625, # ifrit's cauldron
+    208: 6628, # quicksand caves
+    212: 6632, # gustav tunnel
+    213: 6633, # labyrinth of onzozo
+}
